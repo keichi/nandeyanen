3e8af2ea12c6c6e0786144e2180731dc9e8b2bca
Keichi Takahashi <keichi.t@me.com>
2014-07-09 00:41:50 +0900

    Add voice record viewcontroller

---
+++ nandeyanen/NDYRecordViewController.h
@@ -0,0 +1,14 @@
+//
+//  NDYRecordViewController.h
+//  nandeyanen
+//
+//  Created by Keichi Takahashi on 7/9/14.
+//  Copyright (c) 2014 Keichi Takahashi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <AVFoundation/AVFoundation.h>
+
+@interface NDYRecordViewController : UIViewController
+
+@end
