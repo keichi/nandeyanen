9bb3e225ac14163c4d56255ad3fa0bcf33b221ca
Keichi Takahashi <keichi.t@me.com>
2014-07-09 22:38:41 +0900

    Add Groups Table VC

---
+++ nandeyanen/NDYGroupsViewController.h
@@ -0,0 +1,13 @@
+//
+//  NDYGroupsViewController.h
+//  nandeyanen
+//
+//  Created by Keichi Takahashi on 7/9/14.
+//  Copyright (c) 2014 Keichi Takahashi. All rights reserved.
+//
+
+#import <Parse/Parse.h>
+
+@interface NDYGroupsViewController : PFQueryTableViewController
+
+@end
---
+++ nandeyanen/NDYGroupsViewController.m
@@ -0,0 +1,104 @@
+//
+//  NDYGroupsViewController.m
+//  nandeyanen
+//
+//  Created by Keichi Takahashi on 7/9/14.
+//  Copyright (c) 2014 Keichi Takahashi. All rights reserved.
+//
+
+#import "NDYGroupsViewController.h"
+
+@interface NDYGroupsViewController ()
+
+@end
+
+@implementation NDYGroupsViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (id)initWithStyle:(UITableViewStyle)style {
+    self = [super initWithStyle:style];
+    if (self) {
+        // This table displays items in the Todo class
+        self.parseClassName = @"User";
+        self.pullToRefreshEnabled = YES;
+        self.paginationEnabled = NO;
+        self.objectsPerPage = 25;
+    }
+    return self;
+}
+
+- (PFQuery *)queryForTable {
+    PFQuery *query = [PFUser query];
+    
+    // If no objects are loaded in memory, we look to the cache
+    // first to fill the table and then subsequently do a query
+    // against the network.
+    if ([self.objects count] == 0) {
+        query.cachePolicy = kPFCachePolicyCacheThenNetwork;
+    }
+    
+    [query orderByDescending:@"createdAt"];
+    
+    return query;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView
+         cellForRowAtIndexPath:(NSIndexPath *)indexPath
+                        object:(PFObject *)object {
+    static NSString *CellIdentifier = @"Cell";
+    
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleSubtitle
+                                      reuseIdentifier:CellIdentifier];
+    }
+    
+    // Configure the cell to show todo item with a priority at the bottom
+    cell.textLabel.text = [object objectForKey:@"text"];
+    cell.detailTextLabel.text = [NSString stringWithFormat:@"username: %@",
+                                 [object objectForKey:@"username"]];
+    
+    return cell;
+}
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    [self performSegueWithIdentifier:@"start" sender:self];
+}
+
+- (IBAction)returnActionForSegue:(UIStoryboardSegue*)segue
+{
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
